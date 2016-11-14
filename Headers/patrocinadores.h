539416eb0f9822f0264051192d72b2b45132db31
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-13 19:59:45 -0500

    Update patrocinadores.h

--- Headers/patrocinadores.h
+++ Headers/patrocinadores.h
@@ -1,6 +1,12 @@
-Class Patrocinadores{
+class Patrocinadores{
 private:
 	char* name;
 	int ingresos;
+public:
+	Patrocinadores();
+	char* getName();
+	int getIngresos();
+	void setName(char* neim);
+	void setIngresos(int ing);
 	
 };
