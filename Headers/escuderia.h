af2f5132f68b06b97f0ebcd13eeba87e6b7d3344
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-13 19:58:56 -0500

    Update escuderia.h

--- Headers/escuderia.h
+++ Headers/escuderia.h
@@ -1,23 +1,30 @@
+class Persona;
+class Patrocinadores;
+class Car;
 class Escuderia{
 private:
-	Persona *Pilotos; //puntero a array de pilotos
-	int numPilotos;
-	Patrocinador *Sponsors; //puntero a array de Sponsors
+	//Persona *Pilotos; //puntero a array de pilotos
+	//int numPilotos;
+	Patrocinadores *Sponsors; //puntero a array de Sponsors
 	int numSponsors;
 	char* name;
-	Persona JefeDeIngenieros;
-	Persona *Mecanicos; //puntero a array de Mecanicos
-	int numMecanicos;
-	Car carro;
+	Persona JefeDeIngenieros();
+	//Persona *Mecanicos; //puntero a array de Mecanicos
+	//int numMecanicos;
+	Car Carro();
 	//agregar mas miembros privados
 public:
 	Escuderia();
-	Escuderia(Persona Drivers[], int numP, Patrocinadores Pat[], int numS, char* neim, Persona Jefe, Persona Mecs[], int numM, Car car);
+	//Escuderia(Persona Drivers[], int numP, Patrocinadores Pat[], int numS, char* neim, Persona Jefe, Persona Mecs[], int numM, Car car);
 	void setPilotos(Persona Driver);
-	void setSponsor(Patrocinador Sponsor);
+	void setSponsor(Patrocinadores Sponsor);
 	void setName(char* neim);
 	void setJefe(Persona Jefe);
 	void setMecanicos(Persona Mecanico);
 	void setCarro(Car car);
+	char* getName();
+	Persona getJefito();
+	Car getCarro();
+	Patrocinadores getSponsor(int pos);
 	
 };
