a364aceffea2106372778adc5fdb8388bbee216a
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-13 19:58:26 -0500

    Update car.h

--- Headers/car.h
+++ Headers/car.h
@@ -1,9 +1,19 @@
+class Escuderia;
 class Car{ //clase hija de piloto
 private:
 	char* name;
 	char* engine;
 	Escuderia Team;
 	int number;
-	
+public:
+	Car();
+	void setName(char* neim);
+	void setEngine(char* eng);
+	void setEscuderia(Escuderia tim);
+	void setNumber(int num);
+	char* getName();
+	char* getEngine();
+	Escuderia getEscuderia();
+	int getNumber();
 	
 };
