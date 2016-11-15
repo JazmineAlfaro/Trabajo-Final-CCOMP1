d4ebc9de0b37a0d3d6bab1576ebd72a26302c4fe
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-14 20:05:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,48 @@
 #include<iostream>
 #include "persona.h"
+#include "carro.h"
+#include "patrocinadores.h"
+#include "escuderia.h"
+#include "piloto.h"
 using namespace std;
 
 int main () {
-	Persona Jaz;
-	Jaz.setAge(17);
-	Jaz.setName("Yasmeen");
-	Jaz.setID(72193232);
-	Jaz.setCountry("Peru");
-	Jaz.setSex('F');
-	cout << Jaz.getAge() << endl;
-	cout << Jaz.getSex() << endl;
+	Persona Max;
+	Max.setName("Gonzalo Max");
+	Max.setAge(34);
+	Max.setCountry("Namibia");
+	Max.setSex('T');
+	Escuderia ToroAzul;
+	Car Berde;
+	Patrocinadores ZenSex;
+	ToroAzul.setName("Toro Azul");
+	Berde.setName("Berde");
+	Berde.setEngine("Tico");
+	ToroAzul.setCarro(Berde);
+	ZenSex.setName("Condones ZenSex");
+	ToroAzul.setSponsor(ZenSex);
+	Patrocinadores Test;
+	Test = ToroAzul.getSponsor(0);
+	cout << "Item de la lista de Sponsors de la escuderia: " << Test.getName() << endl;
+	Piloto Maxi;
+	Maxi.setName("Max Verstappen mi dios");
+	Maxi.setCountry("Holanda");
+	Maxi.setNumber(33);
+	Maxi.setAge(19);
+	Maxi.setPay(500000);
+	cout << "Nombre del piloto: " << Maxi.getname() << endl;
+	cout << "Puntaje actual del piloto: " << Maxi.getPoints() << endl;
+	cout << "Pais del piloto: " << Maxi.getCountry() << endl;
+	ToroAzul.setPilotos(Maxi);
+	Piloto B;
+	B.setName("Berde");
+	B.setAge(29);
+	B.setSex('F');
+	B.setNumber(69);
+	ToroAzul.setPilotos(B);
+	Piloto Testt;
+	Testt = ToroAzul.getPiloto(1);
+	cout << Testt.getname() << endl;
 	return 0;
 }
+
