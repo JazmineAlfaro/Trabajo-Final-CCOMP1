1ce67df68b909723b1852492251fa164ec57e6c8
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-14 20:02:26 -0500

    Update carro.cpp

--- cpps/carro.cpp
+++ cpps/carro.cpp
@@ -2,19 +2,12 @@
 #include "escuderia.h"
 
 Car::Car(){
-	Escuderia Lel;
 	this->name = " ";
 	this->engine = " ";
-	this->Team = Lel;
-	this->number = 0;
 }
 void Car::setName(char* neim){
 	this->name = neim;
 }
 void Car::setEngine(char* eng){this->engine = eng;}
-void Car::setEscuderia(Escuderia tim){this->Team = tim;}
-void Car::setNumber(int num){this->number = num;}
 char* Car::getName(){return name;}
 char* Car::getEngine(){return engine;}
-Escuderia Car::getEscuderia(){return Team;}
-int Car::getNumber(){return number;}
