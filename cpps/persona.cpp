0a7f86b5d0beefdaea0e48367604fcc1e758d7f2
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-15 22:50:36 -0500

    Update persona.cpp

--- cpps/persona.cpp
+++ cpps/persona.cpp
@@ -1,3 +1,4 @@
+#include<string.h>
 #include "persona.h"
 #include<iostream>
 
@@ -5,36 +6,50 @@ using namespace std;
 Persona::Persona(){
 	this->name = " ";
 	this->age = 0;
-	this->id = 0;
 	this->country = " ";
 	this->sex = 'N'; //undefined sex
 }
 
-Persona::Persona(char* neim, int eish, int aidi, char* contri, char sexo){
+/*Persona::Persona(char* neim, int eish, char* contri, char sexo){
 	this->name = neim;
 	this->age = eish;
-	this->id = aidi;
 	this->country = contri;
 	this->sex = sexo;
-}
+}*/
 //Persona::~Persona();
 int Persona::getAge(){ return age;}
-int Persona::getID(){return id;}
 char* Persona::getname(){return name;}
+//string Persona::getname(){return name;}
 char* Persona::getCountry(){return country;}
+//string Persona::getCountry(){return country;}
 char Persona::getSex(){return sex;}
 void Persona::setAge(int eish){
 	this->age = eish;
 }
-void Persona::setID(int aidi){
-	this->id = aidi;
-}
 void Persona::setName(char* neim){
-	this->name = neim;
+//void Persona::setName(string neim){
+this->name = neim;
 }
 void Persona::setCountry(char* contri){
+//void Persona::setCountry(string contri){
 	this->country = contri;
 }
 void Persona::setSex(char sexo){
 	this->sex = sexo;
+}
+void Persona::printSex(){
+	switch(sex){
+case 'N':
+	cout << "Undefined sex" << endl;
+	break;
+case 'F':
+	cout << "Female" << endl;
+	break;
+case 'M':
+	cout << "Male" << endl;
+	break;
+default:
+	cout << "Sex not recognized" << endl;
+	break;
+	}
 }
