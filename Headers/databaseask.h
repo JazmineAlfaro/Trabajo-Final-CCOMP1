68564a934325cb3b5e83ceecf88325bd73c4c0a1
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-17 09:30:11 -0500

    Create databaseask.h

---
+++ Headers/databaseask.h
@@ -0,0 +1,19 @@
+class DatabaseAsk{
+public:
+	char* askName();
+	char* askLName();
+	int askAge();
+	char askSex();
+	char* askCountry();
+	//CHAU PERSONA
+	//Piloto
+	int askPoints();
+	int askPay();
+	int askNumber();
+	//CHAU PILOTO
+	//Carro
+	char* askEngine();
+	//CHAU CARRO
+	//SPONSOR
+	int askIngresos();
+};
