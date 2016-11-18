f9ecf3cd31f6b68f51791dd46a76746d6b1a59b9
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-18 09:57:53 -0500

    Update databaseask.cpp

--- cpps/databaseask.cpp
+++ cpps/databaseask.cpp
@@ -1,19 +1,79 @@
-class DatabaseAsk{
-public:
-	char* askName();
-	char* askLName();
-	int askAge();
-	char askSex();
-	char* askCountry();
+#include "databaseask.h"
+#include <string.h>
+#include <iostream>
+using namespace std;
+char* DatabaseAsk::askName(){
+	char *name;
+	name = new char[15];
+	cout<<"Ingrese un nombre: "<<endl;
+	cin>>name;
+	return name;
+}
+
+char* DatabaseAsk::askLName(){
+	char *name;
+	name = new char[15];
+	cout<<"Ingrese un apellido: "<<endl;
+	cin>>name;
+	return name;
+}
+int DatabaseAsk::askAge(){
+	int age;
+	cout<<"Ingrese su edad: "<<endl;
+	cin>>age;
+	return age;
+}
+char DatabaseAsk::askSex(){
+	char sex;
+	cout<<"Ingrese su sexo: "<<endl;
+	cin>>sex;
+	return sex;
+}
+
+char* DatabaseAsk::askCountry(){
+	char *name;
+	name = new char[15];
+	cout<<"Ingrese su pais: "<<endl;
+	cin>>name;
+	return name;
+}
 	//CHAU PERSONA
 	//Piloto
-	int askPoints();
-	int askPay();
-	int askNumber();
+int DatabaseAsk::askPoints(){
+	int points;
+	cout<<"Ingrese sus puntos(si no hay ingrese 0): "<<endl;
+	cin>>points;
+	return points;
+}
+int DatabaseAsk::askPay(){
+	int pay;
+	cout<<"Ingrese su paga: "<<endl;
+	cin>>pay;
+	return pay;
+}
+int DatabaseAsk::askNumber(){
+	int number;
+	cout<<"Ingrese su numero: "<<endl;
+	cin>>number;
+	return number;
+
+}
+
 	//CHAU PILOTO
 	//Carro
-	char* askEngine();
+char* DatabaseAsk::askEngine(){
+	char *engine;
+	name = new char[10];
+	cout<<"Ingrese su motor: "<<endl;
+	cin>>engine;
+	return engine;
+}
+				
 	//CHAU CARRO
 	//SPONSOR
-	int askIngresos();
-};
+int DatabaseAsk::askIngresos(){
+	int ingresos;
+	cout<<"Ingrese sus ingresos: "<<endl;
+	cin>>ingresos;
+	return ingresos;
+}
