139079b3e5520900ca07d4ba6890fcbd9f9a000c
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-20 17:28:39 -0500

    Update carro.cpp

--- cpps/carro.cpp
+++ cpps/carro.cpp
@@ -1,6 +1,7 @@
 #include "carro.h"
 #include "escuderia.h"
-
+#include <iostream>
+using namespace std;
 Car::Car(){
 	this->name = " ";
 	this->engine = " ";
@@ -11,3 +12,7 @@ void Car::setName(char* neim){
 void Car::setEngine(char* eng){this->engine = eng;}
 char* Car::getName(){return name;}
 char* Car::getEngine(){return engine;}
+void Car::imprimirCarro(){
+	cout << "Coche:  " << name << endl;
+	cout << "Motor:  " << engine << endl;
+}
