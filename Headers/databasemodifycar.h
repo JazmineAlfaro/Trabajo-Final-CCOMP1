2367b26799431f37e11285fe2344bf6e6f933803
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-22 00:56:31 -0500

    Create databasemodifycar.h

---
+++ Headers/databasemodifycar.h
@@ -0,0 +1,10 @@
+class Car;
+class DatabaseModifyCar{
+public:
+	int chooseCars();
+	int selectDataCars();
+	void modificarNombre(Car P, int i);
+	void modificarMotor(Car P, int i);
+	void selecmodificacion(int i, int k, Car p);
+	void modifyCar();
+};
