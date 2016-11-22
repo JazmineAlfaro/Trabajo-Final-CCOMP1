fc1ea66ed4fe7a927907dd78b820832a215604ea
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-22 01:08:36 -0500

    Create databasemodifyspon.h

---
+++ Headers/databasemodifyspon.h
@@ -0,0 +1,10 @@
+class Patrocinadores;
+class DatabaseModifySpon{
+public:
+	int chooseSpon();
+	int selectDataSpon();
+	void modificarNombre(Patrocinadores P, int i);
+	void modificarIngresos(Patrocinadores P, int i);
+	void selecmodificacion(int i, int k, Patrocinadores p);
+	void modifySpon();
+};
