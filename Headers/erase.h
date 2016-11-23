4f7887e288a8116587d5ad16265d98afc24b5a1f
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-22 22:19:25 -0500

    Create erase.h

---
+++ Headers/erase.h
@@ -0,0 +1,14 @@
+class Erase{
+public:
+	int choosePersona();
+	int choosePiloto();
+	int chooseCoche();
+	int chooseEscuderia();
+	int chooseSponsor();
+	void erasePersona();
+	void erasePiloto();
+	void eraseCoche();
+	void eraseEscuderia();
+	void eraseSponsor();
+	
+};
