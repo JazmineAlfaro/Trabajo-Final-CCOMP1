689a7c91911b6d9e795ce742cebde5413ea1cb8b
jaz <jazmine.alfaro@ucsp.edu.pe>
2016-11-25 10:40:29 -0500

    Update persona.h

--- Headers/persona.h
+++ Headers/persona.h
@@ -1,28 +1,20 @@
 class Persona{
-//protected:
+
 protected:
 	char* name;
-	//string name;
 	char* lastname;
 	int age;
 	char* country; //nationality
-	//string country;
 	char sex;
 public:
 	Persona();
-	//Persona(char* neim, int eish, char* contri, char sexo);
-	//~Persona();
 	int getAge();
 	char* getname();
-	//string getname();
 	char* getCountry();
-	//string getCountry();
 	char getSex();
 	void setAge(int eish);
 	void setName(char* neim);
-	//void setName(string neim);
 	void setCountry(char* contri);
-	//void setCountry(string contri);
 	void setSex(char sexo);
 	void setLastName(char* apellido);
 	void printSex();
