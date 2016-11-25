9d30c6f2de074c39fc9309d9a14fdf8424b613db
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-24 22:22:05 -0500

    Create databasemodifyescu.h

---
+++ Headers/databasemodifyescu.h
@@ -0,0 +1,11 @@
+class Escuderia;
+class DatabaseModifyEscu{
+public:
+	int chooseEscuderias();
+	int selectDataEscuderias();
+	void modificarNombre(Escuderia, int i);
+	void agregarPiloto(Escuderia, int i);
+	void agregarSponsor(Escuderia, int i);
+	void selecmodificacion(int i, int k, Escuderia p);
+	void modifyEscuderia();
+};
