502fd13925360fae23d79e53a0a9d1e7b6b7468f
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-24 23:32:34 -0500

    Update erase.cpp

--- cpps/erase.cpp
+++ cpps/erase.cpp
@@ -33,7 +33,7 @@ int Erase::chooseCoche(){
 }
 int Erase::chooseEscuderia(){
 	Database H; int i;
-	H.getCarro();
+	H.getEscuderia();
 	cout << "Ingrese el numero de la escuderia que desea eliminar: " << endl;
 	cin >> i;
 	return i;
@@ -44,6 +44,16 @@ int Erase::chooseSponsor(){
 	cout << "Ingrese el numero del patrocinador que desea eliminar: " << endl;
 	cin >> i;
 	return i;
+}
+bool Erase::borrarEscuderia(){
+	char s;
+	cout << "Desea borrar toda una escuderia? (s/n)" << endl;
+	cin >> s;
+	if (s == 's' || s == 'S'){
+		return true;
+	} else {
+		return false;
+	}
 }
 void Erase::erasePersona(){
 	Database H;
@@ -64,11 +74,34 @@ void Erase::eraseCoche(){
 	H.deleteVectorCar(i-1);
 }
 void Erase::eraseEscuderia(){
-	;
+	Database H;
+	Escuderia Temp;
+	int i, j;
+	i = chooseEscuderia();
+	if (borrarEscuderia()){
+		H.deleteVectorEscuderia(i-1);
+	} else {
+		cout << "Ingrese 1 (Pilotos) o 2 (Patrocinadores)" << endl;
+		cin >> j;
+		int k;
+		Temp = H.getEscuderiaVector(i-1);
+		if (j == 1){
+			Temp.imprimirPilotos();
+			cout << "Ingrese el numero del piloto que quiera eliminar." << endl;
+			cin >> k;
+			Temp.eliminarPilotos(k-1);
+		} else {
+			Temp.imprimirSponsors();
+			cout << "Ingrese el numero del patrocionador que quiera eliminar." << endl;
+			cin >> k;
+			Temp.eliminarSponsors(k-1);
+		}
+	}
 }
 void Erase::eraseSponsor(){
 	Database H;
 	int i ;
 	i = chooseSponsor();
-	H.deleteVectorSponsor();
+	H.deleteVectorSponsor(i);
 }
+
