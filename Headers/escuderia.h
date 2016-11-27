4b621129f11334a3c9a462cf8d680c4f383b7792
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-27 11:37:12 -0500

    Update escuderia.h

--- Headers/escuderia.h
+++ Headers/escuderia.h
@@ -1,14 +1,15 @@
+#include<iostream>
+#include<vector>
 #include <string>
-
 class Persona;
 class Piloto;
 class Patrocinadores;
 class Car;
 class Escuderia{
 private:
-	Piloto *Pilotos; //puntero a array de pilotos
+	std::vector<Piloto> pilotos;
 	int numPilotoss;
-	Patrocinadores *Sponsors; //puntero a array de Sponsors
+	std::vector<Patrocinadores> sponsors;
 	int numSponsorss;
 	std::string name;
 	Car carro;
@@ -23,11 +24,10 @@ public:
 	void setMecanicos(Persona Mecanico);
 	void setCarro(Car car);
 	std::string getName();
-	Persona getJefito();
 	Car getCarro();
 	Patrocinadores getSponsor(int pos);
 	Piloto getPiloto(int pos);
 	void imprimirSponsors();
 	void imprimirPilotos();
-	
 };
+
